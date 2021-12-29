ff3e34ba7384ab3aa4017f9f7cb5c87c4ce06aee
Filip <filip.ahlander@gmail.com>
2021-12-29 13:50:01 +0100

    add new songs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ console.log(songs.length);
 function App() {
   return (
     <div className="App">
-      <div className="container mx-auto mt-9">
+      <h1 className='font-serif text-2xl font-bold p-9 tracking-widest hover:motion-safe:animate-bounce'>Song hero</h1>
+      <div className="container mx-auto mt-9 justify-center flex-row">
         {songs.map((song, index) => <Card props={song} key={index} />)}
       </div>
       
