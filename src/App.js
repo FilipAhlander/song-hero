6caf023f628a74af13c20189fcc678a28f5b0b9a
Filip <filip.ahlander@gmail.com>
2021-12-29 22:34:32 +0100

    move header

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ function App() {
   const [filter, setFilter] = useState('');
   return (
     <div className="App">
-      <h1 className='font-serif text-2xl font-bold p-9 tracking-widest hover:motion-safe:animate-bounce'>Song hero</h1>
-      <form>
-        <input
-        className='shadow-xl ml-2 rounded-md'
-          type="text"
-          value={filter}
-          name="filter"
-          onChange={(event) => setFilter(event.target.value)}/>
-          <label for="filter" className='ml-2' onClick={() => setFilter('')}>&#8592; Filter</label>
-      </form>
       <div className="container mx-auto mt-9 justify-center flex-row">
+      <h1 className='font-serif text-2xl font-bold p-9 tracking-widest hover:motion-safe:animate-bounce'>Song hero</h1>
+        <form>
+          <input
+          className='shadow-xl ml-2 rounded-md'
+            type="text"
+            value={filter}
+            name="filter"
+            onChange={(event) => setFilter(event.target.value)}/>
+            <label for="filter" className='ml-2' onClick={() => setFilter('')}>&#8592; Filter</label>
+        </form>
         {songs
           .sort((a, b) => (a.artist > b.artist) ? 1 : -1)
           .filter((song) => (
